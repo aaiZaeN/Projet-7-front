a77aa6d023759d71946ac9f0ac9c3fcfc0482dae
aaizaen <max.concept89@gmail.com>
2020-10-08 13:57:13 +0200

    maj

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { LoginForm } from './App/LoginForm';
+import { Site } from './App/Site';
+//import Footer from './App/Footer';
+//import { SignupForm } from './App/SignupForm';
+//import Error from './App/Error';
+//import { Groupoposts } from './App/Groupoposts/Groupoposts';
+//import Profil from './App/Profil'
+
+
+
+//import { SignupForm } from './App/SignupForm';
+
+export default function App() {
+ const [user, setUser] = React.useState(null)
+
+
+  return (
+    //user ? <Site /> : <LoginForm onConnect={setUser} />
+    //user ? <Site /> : <SignupForm onConnect={setUser} />
+   <Site />
+   //<Profil />
+  );
+}
+
+
+
