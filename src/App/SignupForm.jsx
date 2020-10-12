10665e10c3d299590b63c27ee99a96a0ee735e5d
aaizaen <max.concept89@gmail.com>
2020-10-12 15:25:29 +0200

    mise en place du formulaire d'inscription + test d'inscription d'un utilisateur : OK

--- src/App/SignupForm.jsx
+++ src/App/SignupForm.jsx
@@ -1,16 +1,24 @@
 import React, { useState } from 'react'
 import PropTypes from 'prop-types';
 import { ApiErrors, apiFetch } from './utils/api';
-
+import { Button } from './ui/Button';
+ 
 export function SignupForm ({onConnect}) {
   const [error, setError] = useState(null)
   const [loading, setLoading] = useState(false)
+  const [lastName, setLastName] = useState('')
+  const [firstName, setFirstName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
 
   const handleSubmit = async function (e) {
     setError(null)
     setLoading(true)
     e.preventDefault()
-    const data = new FormData(e.target);
+    //Appel de l'API pour envoie (POST)
+    const data = JSON.stringify({lastName, firstName, email, password})
+    console.log(data)
+    console.log(e.target)
     try {
       const user = await apiFetch('/user/register', {
         method: 'POST',
@@ -26,30 +34,32 @@ export function SignupForm ({onConnect}) {
       setLoading(false)
     }
   }
-  
-  // formulaire d'inscription
-  return <form className="container mt-4" onSubmit={handleSubmit}>
+
+  // formulaire de connection
+  return <form className="container mt-4">
     <h2>S'inscrire</h2>
     {error && <Alert>{error}</Alert>} 
     <div className="form-group">
       <label htmlFor="lastName">Nom</label>
-      <input type="text" name="lastName" id="lastName" className="form-control" required/>
+      <input type="text" name="lastName" id="lastName" className="form-control" value={lastName}  onChange={e => setLastName(e.target.value)}required/>
     </div>
     <div className="form-group">
       <label htmlFor="firstName">Prénom</label>
-      <input type="firstName" name="firstName" id="firstName" className="form-control" required/>
+      <input type="text" name="firstName" id="firstName" className="form-control" value={firstName}  onChange={e => setFirstName(e.target.value)}required/>
     </div>
     <div className="form-group">
       <label htmlFor="email">Email</label>
-      <input type="email" name="email" id="email" className="form-control" required/>
+      <input type="text" name="email" id="email" className="form-control" value={email}  onChange={e => setEmail(e.target.value)}required/>
     </div>
     <div className="form-group">
       <label htmlFor="password">Mot de passe</label>
-      <input type="password" name="password" id="password" className="form-control" required/>
+      <input type="password" name="password" id="password" className="form-control" value={password} onChange={e => setPassword(e.target.value)} required/>
     </div>
-    <button type="submit" className="btn btn-primary">S'inscrire</button>
+  
+    <Button type="submit" loading={loading} onClick={handleSubmit}>S'inscrire</Button>
+    <Button type="button">Connection</Button>
   </form>
-} 
+  } 
 
 SignupForm.propTypes = {
   onConnect: PropTypes.func.isRequired
